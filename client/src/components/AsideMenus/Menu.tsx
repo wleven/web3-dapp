f969210f046ca8673d19369b725ee522b010811b
wleven <408493323@qq.com>
2022-08-20 20:48:18 +0800

    feat: onClick

--- client/src/components/AsideMenus/Menu.tsx
+++ client/src/components/AsideMenus/Menu.tsx
@@ -1,6 +1,8 @@
+import React from "react";
 
 interface IProps extends IComProps {
   title?: string;
+  onClick?: React.MouseEventHandler;
 }
 
 function Menu(props: IProps) {
@@ -8,6 +10,7 @@ function Menu(props: IProps) {
     <div
       className={`w-[6rem] h-[6rem] rounded-[8px] flex justify-center items-center bg-soft hover:bg-mute cursor-pointer ${props.className}`}
       title={props.title}
+      onClick={props.onClick}
     >
       {props.children}
     </div>
