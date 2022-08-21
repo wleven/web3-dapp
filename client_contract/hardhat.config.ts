18d163a1c4d018638e32f9b0d82ef277856a9dc4
wleven <408493323@qq.com>
2022-08-21 15:51:06 +0800

    update: network config

--- client_contract/hardhat.config.ts
+++ client_contract/hardhat.config.ts
@@ -3,11 +3,18 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.16",
-
+  defaultNetwork: "matic",
   networks: {
     Ganache: {
       url: "HTTP://172.17.192.1:7545",
     },
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: ["130d91305cdd88eea2306974598179b19ca1a1f65c0a0a478d79b54c90f01bc7"],
+    },
+  },
+  etherscan: {
+    apiKey: "JUFEHNAKX45TSY3JCKSNR9WSTM6UGIN74X",
   },
 };
 
