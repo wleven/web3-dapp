7f47544896b295b3fe7eb82bc22cfa648dd9b605
wleven <408493323@qq.com>
2022-08-22 16:54:32 +0800

    update: 关闭严格模式

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,9 +6,9 @@ import App from "./App";
 import "./index.less";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <ConfigProvider componentSize="large">
       <App />
     </ConfigProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
