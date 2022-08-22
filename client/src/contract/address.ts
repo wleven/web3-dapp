f61874c3cef4ddd39ea045ed8a6360ae538f512c
wleven <408493323@qq.com>
2022-08-22 19:06:49 +0800

    update

--- client/src/contract/address.ts
+++ client/src/contract/address.ts
@@ -1,12 +1,8 @@
 const addressMap = new Map<string, string>();
 
+// addressMap.set("PHB", "0x7B6E62ed08713aA257b0fc6181EFD3B05bF25467");
 addressMap.set("PHB", "0xe5c71aE95B93d22082e8c576eBB7b70D6EbAe43e");
 
 export default function GetContractAddress(contractName: string) {
-  return addressMap.get(contractName) ?? '';
-  //   if (addressMap.has(contractName)) {
-  //     return Promise.resolve(addressMap.get(contractName));
-  //   }
-
-  //   return Promise.reject();
+  return addressMap.get(contractName) ?? "";
 }
