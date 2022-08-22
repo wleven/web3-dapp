e51cc8a338d4467c866ad1130bb4d38d4ce51727
wleven <408493323@qq.com>
2022-08-22 18:32:30 +0800

    feat: menu

--- client/src/components/AsideMenus/UserCard.tsx
+++ client/src/components/AsideMenus/UserCard.tsx
@@ -1,3 +1,5 @@
+import { ImportToken } from "@/hooks/useEthers";
+import Button from "../Button";
 import Card from "../Card";
 
 interface IProps {
@@ -15,11 +17,18 @@ function getAddress(address: string) {
 
 function UserCard(props: IProps) {
   return (
-    <Card className={`absolute top-0 right-[8rem] p-[2rem] leading-1 w-[40rem] h-[14rem] rounded-[8px] ${props.show ? "block" : "hidden"}`}>
-      <div className="text-[1.6rem]">
+    <Card className={`absolute bottom-[-8rem] right-[8rem] p-[2rem] leading-1 w-[40rem]  rounded-[8px] ${props.show ? "block" : "hidden"}`}>
+      <div className="text-[1.6rem] font-bold">
         <div className="mb-[1rem]">Address：{getAddress(props.Address)}</div>
         <div className="mb-[1rem]">ETH：{props.ETH}</div>
         <div>PHB：{props.PHB}</div>
+      </div>
+
+      <div className="mt-[2rem]">
+        <Button className="mr-[1rem]" onClick={ImportToken}>
+          导入PHB代币
+        </Button>
+        <Button>领取PHB代币</Button>
       </div>
     </Card>
   );
