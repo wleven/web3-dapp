d41fbdc30beecb7fe062580acea0486c046eae88
wleven <408493323@qq.com>
2022-08-22 23:02:56 +0800

    feat: onClick

--- client/src/components/ProjectItem/index.tsx
+++ client/src/components/ProjectItem/index.tsx
@@ -1,14 +1,13 @@
 import Card from "../Card";
 
-interface IProps {
+interface IProps extends IComProps {
   name: string;
   emoji?: string;
-  children?: React.ReactNode;
 }
 
 function ProjectItem(props: IProps) {
   return (
-    <Card className="text-[3rem] text-center py-[4rem] rounded-[1rem]" hover>
+    <Card className="text-[3rem] text-center py-[4rem] rounded-[1rem]" hover onClick={props.onClick}>
       <div className="text-[6rem] mb-[4rem]">{props.emoji ?? "⚒️"}</div>
       <div>{props.name}</div>
     </Card>
