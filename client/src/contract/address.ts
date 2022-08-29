e1d7947f34c5cad3f5c08ecdaeef4a00c9c7fab8
wleven <408493323@qq.com>
2022-08-29 13:45:11 +0800

    update: style

--- client/src/contract/address.ts
+++ client/src/contract/address.ts
@@ -1,6 +1,5 @@
 const addressMap = new Map<string, string>();
 
-// addressMap.set("PHB", "0x7B6E62ed08713aA257b0fc6181EFD3B05bF25467");
 addressMap.set("PHB", "0xe5c71aE95B93d22082e8c576eBB7b70D6EbAe43e");
 addressMap.set("ToDoList", "0x7413E7F76Af3290A7E6876a45512ef6CAA8f4503");
 
