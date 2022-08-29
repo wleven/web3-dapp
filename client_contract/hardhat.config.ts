4cbe31f5f007e1d4ee47b5c5b1f1dee16e524fca
wleven <408493323@qq.com>
2022-08-29 14:00:19 +0800

    update: networks

--- client_contract/hardhat.config.ts
+++ client_contract/hardhat.config.ts
@@ -8,13 +8,6 @@ const config: HardhatUserConfig = {
     Ganache: {
       url: " http://172.25.192.1:7545/",
     },
-    matic: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: ["130d91305cdd88eea2306974598179b19ca1a1f65c0a0a478d79b54c90f01bc7"],
-    },
-  },
-  etherscan: {
-    apiKey: "JUFEHNAKX45TSY3JCKSNR9WSTM6UGIN74X",
   },
 };
 
